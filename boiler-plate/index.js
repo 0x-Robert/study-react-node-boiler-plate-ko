621dbb2d29389ecf3f7ff5af505ba51583a7d2f8
shypang <smartdragon417@gmail.com>
2021-10-28 20:50:40 +0900

    add nodemon

--- boiler-plate/index.js
+++ boiler-plate/index.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+//application/x-www-form-urlencoded 해석
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application/json 해석
+app.use(bodyParser.json());
 
 
 const mongoose = require('mongoose')
@@ -18,8 +26,23 @@ mongoose.connect('mongodb+srv://yongyong:jsidhsbs16@boilerplate.l7j6d.mongodb.ne
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! 안녕하세요 sadfasdfasdfsf' )
 })
+
+app.post('/register', (req,res) => {
+
+  //회원 가입할 때 필요한 정보들을 client에서 가져오면
+  //그것들을 데이터 베이스에 넣어준다.
+  const user = new User(req.body)
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
